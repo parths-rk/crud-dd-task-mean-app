d94c28ced19401647f48ceadafdac75bb65d1959
Parth <parths.singh2003@gmail.com>
2026-02-24 10:13:12 +0000

    Mostly done

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+
 const routes: Routes = [
-  // your routes
+  { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
+  { path: 'tutorials', component: TutorialsListComponent },
+  { path: 'add', component: AddTutorialComponent },
+  { path: 'tutorials/:id', component: TutorialDetailsComponent }
 ];
 
 @NgModule({
